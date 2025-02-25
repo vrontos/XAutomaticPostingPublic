e56d46e409d7dbb107d8b9e71263990160bd6c21
vrontos <56764141+vrontos@users.noreply.github.com>
2025-02-25 13:58:58 +0100

    Update XAutomaticPosting.py

--- XAutomaticPosting.py
+++ XAutomaticPosting.py
@@ -5,6 +5,7 @@ from functions import (
     retrieve_articles,
     call_llm_for_post_creation,
     green_text,
+    blue_text,
     post_on_x
 )
 import random
@@ -23,37 +24,28 @@ print("\n### 3. Chosen prompt style:", prompt_style)
 
 # 4. Decide post length
 post_length = decide_post_length()
-print("\n### 4. Chosen post length:", prompt_style)
+print("\n### 4. Chosen post length:", post_length)
 
+# If needed, retrieve articles; otherwise, set articles to None
+articles = None
 if use_news_api_org:
-    # 5. Call News API Org to retrieve articles
     articles = retrieve_articles(topic)
-    # If no articles are found, stop further execution
     if not articles.strip():
         print("No articles found. Stopping further execution.")
         exit(0)
     print("\n### 5. Retrieved articles:\n", articles)
 
-    # 6. Call Gemini LLM to create an X post
-    post_created_from_llm = call_llm_for_post_creation(prompt_style, topic, post_length, articles)
-    print(f"\n### 6. Generated X post:")
-    print(f"- TOPIC: {topic}")
-    print(f"- LENGTH: {post_length}")
-    print(f"- USED NEWS ARTICLES: {use_news_api_org}")
-    print(f"- STYLES: {prompt_style}\n")
-    green_text(post_created_from_llm)
-
-# If NO need to use the News API Org
-else:
-    # 5. Call Gemini LLM to create an X post
-    post_created_from_llm = call_llm_for_post_creation(prompt_style, topic, post_length, articles=None)
-    print(f"\n### 5. Generated X post:")
-    print(f"- TOPIC: {topic}")
-    print(f"- LENGTH: {post_length}")
-    print(f"- USED NEWS ARTICLES: {use_news_api_org}")
-    print(f"- STYLES: {prompt_style}\n")
-    green_text(post_created_from_llm)
+# 6. Call Gemini LLM to create an X post
+prompt, post_created_from_llm = call_llm_for_post_creation(prompt_style, topic, post_length, articles)
+print("\nPROMPT:")
+blue_text(prompt)
+print(f"\n### Generated X post:")
+print(f"- TOPIC: {topic}")
+print(f"- LENGTH: {post_length}")
+print(f"- USED NEWS ARTICLES: {use_news_api_org}")
+print(f"- STYLES: {prompt_style}\n")
+green_text(post_created_from_llm)
 
 # Post on X
 post_on_x(post_created_from_llm)
-print("Posted successfuly on X!🚀")
+print("Posted successfully on X!🚀")
